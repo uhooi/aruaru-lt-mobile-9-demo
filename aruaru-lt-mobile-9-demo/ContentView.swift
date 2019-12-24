cc23aefac51f9f4b12a34801e340800fcffee8ea
uhooi <uhooi@server.fake>
2019-12-24 14:24:46 +0900

    Xcodeプロジェクトの作成

---
+++ aruaru-lt-mobile-9-demo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  aruaru-lt-mobile-9-demo
+//
+//  Created by uhooi on 2019/12/24.
+//  Copyright © 2019 THE Uhooi. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ aruaru-lt-mobile-9-demoTests/aruaru_lt_mobile_9_demoTests.swift
@@ -0,0 +1,34 @@
+//
+//  aruaru_lt_mobile_9_demoTests.swift
+//  aruaru-lt-mobile-9-demoTests
+//
+//  Created by uhooi on 2019/12/24.
+//  Copyright © 2019 THE Uhooi. All rights reserved.
+//
+
+import XCTest
+@testable import aruaru_lt_mobile_9_demo
+
+class aruaru_lt_mobile_9_demoTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
